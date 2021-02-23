b182a5020f4a602c1ddef209881bb560ad5be218
17-76018348 <53898123+17-76018348@users.noreply.github.com>
2021-02-23 19:57:58 +0900

    Update filetrans.py

--- simulator_VRP/simulator/filetrans.py
+++ simulator_VRP/simulator/filetrans.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import json
 command = 'loadCarrier /home/didrmswp/capstone/simulator_VRP/instance_vrp/rizzo_stguillain_ds-vrptw/OC-100-70-25%-1/12-18-0-0/lockers-no/carrier.json'
 command = command.replace('loadCarrier ', '', 1).rstrip()
 data = None
 with open(command) as data:
     dataLoaded = json.load(data)
-    data = pd.DataFrame.from_dict({(i,j): user_dict[i][j] 
-                           for i in user_dict.keys() 
-                           for j in user_dict[i].keys()},
+    data = pd.DataFrame.from_dict({(i,j): dataLoaded[i][j] 
+                           for i in dataLoaded.keys() 
+                           for j in dataLoaded[i].keys()},
                        orient='index')
 print(data)
