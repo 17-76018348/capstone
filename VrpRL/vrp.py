64bf080aed5dbf881808b4d5149f3d8b8d97b900
RYU-BB <tmxkdd5790@gmail.com>
2021-03-04 20:43:16 +0900

    Update vrp.py

--- VrpRL/vrp.py
+++ VrpRL/vrp.py
@@ -16,7 +16,6 @@ class VrpEnv(object):
     def __init__(self, nodes):
         self.customer_x = nodes['map_coored'][0]
         self.customer_y = nodes['map_coored'][1]
-        self.node_types = nodes['node_types']
         self.capacity = nodes['capacity']
         self.node_size = len(nodes)
         self.depot_x = 40
@@ -40,27 +39,27 @@ class VrpEnv(object):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, action):
-        x, y, capacity, time = self.state
+    def step(self, action, reward):
+        x, y, capacity, time, complete_requests = self.state
         next_x = self.customer_x[action]
         next_y = self.customer_y[action]
         self.node_check[action] = False
 
         next_time = time + (euclidean_distance((x, y), (next_x, next_y)) / self.speed)
 
-        if self.node_types[action] == 'customer':
-            capacity -= 1
-        else:
-            capacity += 1
+        capacity -= self.capacity[action]
 
         self.state = (next_x, next_y, capacity, next_time)
 
         done = bool(
             capacity > self.threshold_capacity
-            or time > self.threshold_time
         )
 
         if not done:
+            if time > self.threshold_time:
+                reward -= 1000
+            if capacity == 0:
+                reward += 1000
             reward += 1 / (euclidean_distance((x, y), (next_x, next_y)))
         else:
             logger.warn(
@@ -70,7 +69,7 @@ class VrpEnv(object):
         return np.array(self.state), reward, done, {}
 
     def reset(self):
-        self.state = (40, 60, 0, 0)
+        self.state = (40, 60, 18, 0)
         self.node_check = [True for _ in range(len(self.node_size))]
         return np.array(self.state)
 
